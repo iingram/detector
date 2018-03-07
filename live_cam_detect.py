0b36e95d2dc81e3783b4e49c8d94e4b08d89fca6
Ian Ingram <ian@ingramclockworks.com>
2018-03-06 16:22:49 -0800

    cleaning up: dropping the extra wrapper function around camera.read(). getting stuff that should be in the body of main in there.

--- live_cam_detect.py
+++ live_cam_detect.py
@@ -3,27 +3,22 @@
 import cv2
 from detector import *
 
-camera_port = 0 
-camera = cv2.VideoCapture(camera_port)
-
-# this section solves a bug where camera's auto-exposure
-# won't have taken action until after a healthy number of frames have
-# been captured
-ramp_frames = 30
-def get_image():
-  retval, im = camera.read()
-  return im
-
-for i in xrange(ramp_frames):
-  temp = get_image()
-### end camera "warm-up" section
-  
 if __name__ == '__main__':
+  camera_port = 0 
+  camera = cv2.VideoCapture(camera_port)
+
+  # solves a bug where camera's auto-exposure won't have taken action
+  # until after a healthy number of frames have been captured
+  ramp_frames = 30
+  for i in xrange(ramp_frames):
+    retval, temp = camera.read()
+  ### end camera "warm-up" section
+
   # Creates graph from saved GraphDef.
   create_graph()
   
   while(1):
-    camera_capture = get_image()
+    retval, camera_capture = camera.read()
     file = "test_image.png"
     cv2.imwrite(file, camera_capture)
     vote = detector('test_image.png')
