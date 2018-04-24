02be0a64df373acb311f0f7e1a01c2336beb0e51
Loaner <Loaner@WL-213-187.WPA.Claremont.Edu>
2018-04-24 16:40:30 -0700

    Added brightest region detection

--- obj_tracking.py
+++ obj_tracking.py
@@ -5,23 +5,39 @@ import sys
 import os
 import cv2
 
-# Get webcam video
-cap = cv2.VideoCapture(0)
+CIRCLE_DIAMETER_PIXELS = 100
 
-# Initialize background filter
-fsbg = cv2.BackgroundSubtractorMOG()
+def findBrightestSpot(img):
+    gray = cv2.GaussianBlur(img, (5, 5), 1)
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+    image = img.copy()
+    cv2.circle(image, maxLoc, CIRCLE_DIAMETER_PIXELS, (255, 0, 0), 2)
 
-while(1):
-    # Continually apply filter to camera
-    ret, frame = cap.read()
-    fgmask = fsbg.apply(frame)
-
-    # Show results
-    cv2.imshow('Frame', fgmask)
-
+    # display the results of our newly improved method
+    cv2.imshow("Robust", image)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
-        break
+        return
 
-cap.release()
-cv2.destroyAllWindows()
+def main():
+    # Get webcam video
+    cap = cv2.VideoCapture(0)
+
+    # Initialize background filter
+    fsbg = cv2.BackgroundSubtractorMOG()
+
+
+    while(1):
+        ret, frame = cap.read()
+        fgmask = fsbg.apply(frame)
+        cv2.imshow('frame',fgmask)
+        k = cv2.waitKey(30) & 0xff
+        findBrightestSpot(fgmask)
+        if k == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+  main()
