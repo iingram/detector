eeceae0ce60e61076b7a776cbb9af84c470c6733
Ian Ingram <ian@ingramclockworks.com>
2018-05-11 12:24:48 +0300

    also switching this to import with more careful namespacing

--- canned_images_detect.py
+++ canned_images_detect.py
@@ -1,6 +1,7 @@
 # runs detector on successive images stored in a directory
 
-from detector import *
+import detector as dt
+import classifier as cl
 import time
 import os
 
@@ -8,14 +9,14 @@ images_dir = '/home/ian/Desktop/test'
 
 def main():
   # Creates graph from saved GraphDef.
-  create_graph()
+  cl.create_graph()
 
   for file in sorted(os.listdir(images_dir), key=str.lower):
     if file.endswith(".jpg") or file.endswith(".JPG"):
       start = time.time() # want to time each cycle. starting stopwatch.
       full_file = os.path.join(images_dir, file)
       print(full_file)
-      vote, target_label = detector(full_file)
+      vote, target_label = dt.detector(full_file)
       print(target_label + ' is present.')
       
       end = time.time()
